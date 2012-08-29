ccbbeaf4f407f3afe13048919160808a603670c5
Zdanovitch Nikita <3hb.tch@gmail.com>
2012-08-29 19:56:52 -0300

    Primer subida de proyecto

---
+++ okhub 0.1/src/mainPkg/Application.java
@@ -0,0 +1,28 @@
+package mainPkg;
+
+import pruebaPkg.*;
+
+public class Application 
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		
+		System.out.print("presione 1 2 o 3 para llamar al metodo run de la clase respectiva: \n");
+		
+		char claseElegida = U.leerChar();
+	
+		switch(claseElegida)
+		{
+		case '1': (new Clase1()).run(); break;
+		case '2': (new Clase2()).run(); break;
+		case '3': (new Clase3()).run(); break;
+		default:  System.out.print("Opcion no valida, saliendo"); break;
+		}
+		
+	}
+
+}
---
+++ okhub 0.1/src/pruebaPkg/Clase3.java
@@ -0,0 +1,12 @@
+package pruebaPkg;
+
+public class Clase3 implements IRuneable
+{
+	public int run()
+	{
+
+		System.out.print("3");
+		
+		return 3;
+	}
+}
