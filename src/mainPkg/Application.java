a07f84841f5dacb680bd7843971ec6bdaa9b9499
GSeva <gloomar2006@hotmail.com>
2012-09-03 20:33:19 -0300

    Prueba de commit

--- src/mainPkg/Application.java
+++ src/mainPkg/Application.java
@@ -7,11 +7,12 @@ public class Application
 
 	/**
 	 * @param args
+	 * Comentario absurdo e innecesario 
 	 */
 	public static void main(String[] args) 
 	{
 		
-		System.out.print("presione 1 2 o 3 para llamar al metodo run de la clase respectiva: \n");
+		System.out.print("presione 1 2 3 o 4 para llamar al metodo run de la clase respectiva: \n");
 		
 		char claseElegida = U.leerChar();
 	
@@ -20,6 +21,7 @@ public class Application
 		case '1': (new Clase1()).run(); break;
 		case '2': (new Clase2()).run(); break;
 		case '3': (new Clase3()).run(); break;
+		case '4': (new Clase4()).run(); break;
 		default:  System.out.print("Opcion no valida, saliedo"); break;
 		}
 		
---
+++ src/pruebaPkg/Clase4.java
@@ -0,0 +1,14 @@
+package pruebaPkg;
+
+public class Clase4 {
+	
+	public int run()
+	{
+		
+		System.out.print("4");
+		
+		return 1;
+		
+	}
+
+}
