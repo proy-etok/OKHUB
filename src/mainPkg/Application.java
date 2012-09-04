8ff6ee34e2f825c23f4fa7dbaf62dc052e7bde08
GSeva <gloomar2006@hotmail.com>
2012-09-03 21:27:59 -0300

    Reverenda prueba de commit

--- src/mainPkg/Application.java
+++ src/mainPkg/Application.java
@@ -12,7 +12,7 @@ public class Application
 	public static void main(String[] args) 
 	{
 		
-		System.out.print("presione 1 2 3 o 4 para llamar al metodo run de la clase respectiva: \n");
+		System.out.print("presione 1 2 3 o 4 o 5 para llamar al metodo run de la clase respectiva: \n");
 		
 		char claseElegida = U.leerChar();
 	
@@ -22,6 +22,7 @@ public class Application
 		case '2': (new Clase2()).run(); break;
 		case '3': (new Clase3()).run(); break;
 		case '4': (new Clase4()).run(); break;
+		case '5': System.out.print("Opcion valida"); break;
 		default:  System.out.print("Opcion no valida, saliedo"); break;
 		}
 		
