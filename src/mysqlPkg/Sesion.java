228128db91e7496dd55e279d09357725d86415fa
Zdanovitch Nikita <3hb.tch@gmail.com>
2012-09-12 18:48:11 -0300

    Nikita
    Creada Sesion.java

---
+++ src/mysqlPkg/Sesion.java
@@ -0,0 +1,24 @@
+package mysqlPkg;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+public class Sesion
+{
+	private String conString = "jdbc:mysql://190.192.77.168:3306/okhub";
+	
+	private Connection con = null;
+    private Statement st = null;
+    private ResultSet rs = null;
+    
+	public OKHConn()
+	{
+		
+		
+	}
+
+}
