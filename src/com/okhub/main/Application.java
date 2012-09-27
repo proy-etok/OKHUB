d88bb46c1de1915e177763e448630d7d7ed070f9
GSeva <gloomar2006@hotmail.com>
2012-09-27 20:37:36 -0300

    Creado un nuevo package com.okhub.main

---
+++ src/com/okhub/main/Application.java
@@ -0,0 +1,22 @@
+package com.okhub.main;
+
+import com.okhub.gui.*;
+
+
+public class Application 
+{
+
+	/**
+	 * @param args
+	 * Comentario absurdo e innecesario 
+	 */
+	public static void main(String[] args) 
+	{
+		(new Ventana_Login_Utilidad()).Crear_Ventana_Login();
+		/*
+		Ventana_Login vl = new Ventana_Login();
+		vl.Crear_Ventana_Login();
+		*/
+	}
+
+}
